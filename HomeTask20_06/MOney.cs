c939aa47fea0bf7b032dfbc523620e84801d8524
sarvar <githup0303@gmail.com>
2023-06-22 09:29:22 +0500

    Add project files.

---
+++ HomeTask20_06/MOney.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTask20_06
+{
+    public class MOney
+    {
+        public delegate decimal CurrencyConversion(decimal amount);
+
+        public class CurrencyConverter
+        {
+            private Dictionary<string, decimal> rates;
+
+            public CurrencyConverter()
+            {
+                // Valyuta kurslari:
+                rates = new Dictionary<string, decimal>()
+            {
+                {"EUR", 0.000083m}, // Misol uchun, 1 UZS = 0.000083 EUR
+                {"USD", 0.000087m},  // Misol uchun, 1 UZS = 0.000087 USD
+                {"RUB", 0.009m},   // Misol uchun, 1 UZS = 0.009 RUB
+                {"GBP", 0.000071m}, // Misol uchun, 1 UZS = 0.000071 GBP
+                {"CHF", 0.000082m}, // Misol uchun, 1 UZS = 0.000082 CHF
+                {"JPY", 0.014m}, // Misol uchun, 1 UZS = 0.014 JPY
+                {"KZT", 0.067m} // Misol uchun, 1 UZS = 0.067 KZT
+                
+
+            };
+            }
+
+            public CurrencyConversion GetConversion(string currency)
+            {
+                if (rates.ContainsKey(currency))
+                    return natija => natija * rates[currency];
+                else
+                    throw new ArgumentException($"konvertatsiya darajasi mavjud emas {currency}");
+            }
+        }
+
+        
+    }
+
+}
---
+++ HomeTask20_06/Program.cs
@@ -0,0 +1,17 @@
+﻿using static HomeTask20_06.MOney;
+
+Console.WriteLine("Hello, World!");
+
+
+var converter = new CurrencyConverter();
+Console.WriteLine("Valyuta kurslariga baholamoqchi bolgan somingizni kiriting:");
+decimal  UZS = decimal.Parse(Console.ReadLine());
+
+var EUR = converter.GetConversion("EUR");
+var USD = converter.GetConversion("USD");
+var RUB = converter.GetConversion("RUB");
+
+Console.WriteLine($"{UZS} UZS ga teng {EUR(UZS)} EUR");
+Console.WriteLine($"{UZS} UZS ga teng {USD(UZS)} USD");
+Console.WriteLine($"{UZS} UZS ga teng {RUB(UZS)} RUB");
+
